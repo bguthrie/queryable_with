c9f62c760e7a239fef2ea72d3c531209a475e9b5
Brian Guthrie <btguthrie@gmail.com>
2010-05-16 00:18:18 +0530

    Adding basic gem build, clean, and release tasks. Correcting version.

--- lib/queryable_with/version.rb
+++ lib/queryable_with/version.rb
@@ -1,8 +1,8 @@
-module ResourceFull #:nodoc:
+module QueryableWith #:nodoc:
   module Version #:nodoc:
     MAJOR = 0
-    MINOR = 7
-    TINY  = 9
+    MINOR = 1
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
